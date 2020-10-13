1bbb0adc1058c47055ae58b39d48d15343e38c93
brocEdson <bedson@mail.bradley.edu>
2020-10-13 01:50:03 -0500

    Added animations

--- PennyPixel_2DTilemapProject/Assets/PennyPixel/Scripts/PlayerPlatformerController.cs
+++ PennyPixel_2DTilemapProject/Assets/PennyPixel/Scripts/PlayerPlatformerController.cs
@@ -1,4 +1,8 @@
-﻿using System.Collections;
+﻿/* Broc Edson
+ * Assignment 5
+ * Controls player movement
+ */
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -49,6 +53,7 @@ public class PlayerPlatformerController : PhysicsObject {
 
         animator.SetBool ("grounded", grounded);
         animator.SetFloat ("velocityX", Mathf.Abs (velocity.x) / maxSpeed);
+        animator.SetFloat("velocityY", velocity.y);
 
         targetVelocity = move * maxSpeed;
     }
