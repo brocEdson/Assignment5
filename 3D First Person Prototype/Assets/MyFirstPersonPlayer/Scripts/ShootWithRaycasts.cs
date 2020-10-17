aa6d0bce5d3145bf4f8c2563d43a14c1d9e6c1c1
brocEdson <bedson@mail.bradley.edu>
2020-10-17 16:44:54 -0500

    Added many ProBuilder tests

--- 3D First Person Prototype/Assets/MyFirstPersonPlayer/Scripts/ShootWithRaycasts.cs
+++ 3D First Person Prototype/Assets/MyFirstPersonPlayer/Scripts/ShootWithRaycasts.cs
@@ -6,7 +6,9 @@ public class ShootWithRaycasts : MonoBehaviour
 {
     public float damage = 10f;
     public float range = 100f;
+    public float hitForce = 10f;
     public Camera cam;
+    public ParticleSystem muzzleFlash;
 
     void Update()
     {
@@ -18,10 +20,21 @@ public class ShootWithRaycasts : MonoBehaviour
 
     void Shoot()
     {
+        muzzleFlash.Play();
         RaycastHit hitInfo;
-        if(Physics.Raycast(cam.transform.position, cam.transform.forward, out hitInfo, range))
+        if (Physics.Raycast(cam.transform.position, cam.transform.forward, out hitInfo, range))
         {
             Debug.Log(hitInfo.transform.gameObject.name);
+
+            Target target = hitInfo.transform.gameObject.GetComponent<Target>();
+            if(target != null)
+            {
+                target.TakeDamage(damage);
+            }
+            if(hitInfo.rigidbody != null)
+            {
+                hitInfo.rigidbody.AddForce(cam.transform.TransformDirection(Vector3.forward) * hitForce, ForceMode.Impulse);
+            }
         }
     }
 }
