ccd2d8e0e6cab4fdbd9f1138f0e26ee961392123
brocEdson <bedson@mail.bradley.edu>
2020-10-17 15:33:23 -0500

    Added shooty things

---
+++ 3D First Person Prototype/Assets/MyFirstPersonPlayer/Scripts/ShootWithRaycasts.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootWithRaycasts : MonoBehaviour
+{
+    public float damage = 10f;
+    public float range = 100f;
+    public Camera cam;
+
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        RaycastHit hitInfo;
+        if(Physics.Raycast(cam.transform.position, cam.transform.forward, out hitInfo, range))
+        {
+            Debug.Log(hitInfo.transform.gameObject.name);
+        }
+    }
+}
