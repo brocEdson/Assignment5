75a23bc15cbc976f1fe36412be4fdc8db190521d
brocEdson <bedson@mail.bradley.edu>
2020-10-17 21:09:00 -0500

    Finished basic 3D prototype

---
+++ 3D First Person Prototype/Assets/MyFirstPersonPlayer/Scripts/WinOnTrigger.cs
@@ -0,0 +1,19 @@
+﻿/* Broc Edson
+ * Assignment 5
+ * Wins the game when triggered by the end trigger
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinOnTrigger : MonoBehaviour
+{
+    public GameObject winText;
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "EndTrigger")
+        {
+            winText.SetActive(true);
+        }
+    }
+}
